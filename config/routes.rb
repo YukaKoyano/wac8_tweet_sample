a7e7bd94b566c205045081d1661a6595718cc11b
Yuka <yukakoyano@example.com>
2016-09-04 02:26:57 +0000

    Createアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-　resources :tweets
-  
+     resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
